8c15b76718d7a048b0c11f3160bd3d92883823b5
Andrew Poelstra <apoelstra@wpsoftware.net>
2023-01-27 14:52:33 +0000

    remove old command-line commans

--- src/main.rs
+++ src/main.rs
@@ -23,28 +23,13 @@ pub mod trade;
 
 use anyhow::Context;
 use clap::Clap;
-use rust_decimal::prelude::ToPrimitive;
 use rust_decimal::Decimal;
 use std::{convert::TryInto, fs, path::PathBuf};
 
-use price::{BitcoinPrice, Historic};
+use price::Historic;
 
 /// Don't bother loading historical price data from before this date
 const MIN_PRICE_DATE: &str = "2023";
-
-fn from_date(s: &str) -> Result<time::Date, anyhow::Error> {
-    Ok(time::Date::parse(s, "%F")?)
-}
-
-fn from_datetime(s: &str) -> Result<time::PrimitiveDateTime, anyhow::Error> {
-    time::PrimitiveDateTime::parse(s, "%F %T")
-        .or(from_date(s).map(|d| d.with_time(time::time!(21:00))))
-}
-
-fn from_offset_datetime(s: &str) -> Result<time::OffsetDateTime, anyhow::Error> {
-    time::OffsetDateTime::parse(s, "%F %T%z")
-        .or(from_datetime(s).map(|dt| dt.assume_utc().to_offset(time::UtcOffset::UTC)))
-}
 
 #[derive(Clap)]
 enum Command {
@@ -74,56 +59,6 @@ enum Command {
         #[clap(long, short)]
         price: Option<Decimal>,
     },
-    PriceCall {
-        #[clap(long, short)]
-        /// Strike price of the proposed call option
-        strike: Decimal,
-        /// Expiry date of the proposed call option
-        #[clap(long, short, parse(try_from_str = from_offset_datetime))]
-        expiry: time::OffsetDateTime,
-        /// Assumed BTC price volatility
-        #[clap(long, short)]
-        volatility: f64,
-    },
-    ImpliedVolatilityCall {
-        #[clap(long, short)]
-        /// Strike price of the proposed call option
-        strike: Decimal,
-        /// Expiry date of the proposed call option
-        #[clap(long, short, parse(try_from_str = from_offset_datetime))]
-        expiry: time::OffsetDateTime,
-        /// Price
-        #[clap(long, short)]
-        price: Decimal,
-        /// Bitcoin Price, if provided
-        #[clap(long, short)]
-        btc_price: Option<Decimal>,
-    },
-    PricePut {
-        #[clap(long, short)]
-        /// Strike price of the proposed call option
-        strike: Decimal,
-        /// Expiry date of the proposed call option
-        #[clap(long, short, parse(try_from_str = from_offset_datetime))]
-        expiry: time::OffsetDateTime,
-        /// Assumed BTC price volatility
-        #[clap(long, short)]
-        volatility: f64,
-    },
-    ImpliedVolatilityPut {
-        #[clap(long, short)]
-        /// Strike price of the proposed call option
-        strike: Decimal,
-        /// Expiry date of the proposed call option
-        #[clap(long, short, parse(try_from_str = from_offset_datetime))]
-        expiry: time::OffsetDateTime,
-        /// Price
-        #[clap(long, short)]
-        price: Decimal,
-        /// Bitcoin Price, if provided
-        #[clap(long, short)]
-        btc_price: Option<Decimal>,
-    },
 }
 
 fn main() -> Result<(), anyhow::Error> {
@@ -133,6 +68,18 @@ fn main() -> Result<(), anyhow::Error> {
     println!("Trade tracker version [whatever]");
     println!("Price data pulled from http://api.bitcoincharts.com/v1/trades.csv?symbol=bitstampUSD -- call `update-price-data` to update");
     println!("");
+
+    let command = Command::parse();
+    let history = if let Command::InitializePriceData { .. } = command {
+        // unused when initializing price data, just pick something
+        Historic::default()
+    } else {
+        data_path.push("pricedata");
+        let history = Historic::read_json_from(&data_path, MIN_PRICE_DATE)
+            .context("reading price history")?;
+        data_path.pop();
+        history
+    };
 
     match Command::parse() {
         Command::InitializePriceData { csv } => {
@@ -155,10 +102,7 @@ fn main() -> Result<(), anyhow::Error> {
             data_path.pop();
         }
         Command::UpdatePriceData { url } => {
-            data_path.push("pricedata");
-            let mut history = Historic::read_json_from(&data_path, MIN_PRICE_DATE)
-                .context("reading price history")?;
-            data_path.pop();
+            let mut history = history; // lol rust
             let data = minreq::get(&url)
                 .with_timeout(10)
                 .send()
@@ -175,20 +119,10 @@ fn main() -> Result<(), anyhow::Error> {
             data_path.pop();
         }
         Command::LatestPrice {} => {
-            data_path.push("pricedata");
-            let history = Historic::read_json_from(&data_path, MIN_PRICE_DATE)
-                .context("reading price history")?;
-            data_path.pop();
-
             let now = time::OffsetDateTime::now_utc();
             println!("{}", history.price_at(now));
         }
         Command::Price { option, volatility } => {
-            data_path.push("pricedata");
-            let history = Historic::read_json_from(&data_path, MIN_PRICE_DATE)
-                .context("reading price history")?;
-            data_path.pop();
-
             let now = time::OffsetDateTime::now_utc();
             let yte = option.years_to_expiry(&now);
             let current_price = history.price_at(now);
@@ -201,8 +135,8 @@ fn main() -> Result<(), anyhow::Error> {
                 1.0 / yte
             );
             println!("");
-            for vol in 0..21 {
-                let vol = volatility.unwrap_or(0.5) + 0.05 * (vol as f64);
+            for vol in 0..51 {
+                let vol = volatility.unwrap_or(0.5) + 0.02 * (vol as f64);
                 println!(
                     "Vol: {:3.2}   Price ($): {:8.2}   Theta ($): {:5.2}",
                     vol,
@@ -212,11 +146,6 @@ fn main() -> Result<(), anyhow::Error> {
             }
         }
         Command::Iv { option, price } => {
-            data_path.push("pricedata");
-            let history = Historic::read_json_from(&data_path, MIN_PRICE_DATE)
-                .context("reading price history")?;
-            data_path.pop();
-
             let now = time::OffsetDateTime::now_utc();
             let yte = option.years_to_expiry(&now);
             let current_price = history.price_at(now);
@@ -253,159 +182,8 @@ fn main() -> Result<(), anyhow::Error> {
                         option.bs_theta(&now, current_price.btc_price, vol),
                     ),
                 }
-                price += center / Decimal::from(20);
+                price += center / Decimal::from(40);
             }
-        }
-        Command::PriceCall {
-            strike,
-            expiry,
-            volatility,
-        } => {
-            data_path.push("pricedata");
-            let history = Historic::read_json_from(&data_path, MIN_PRICE_DATE)
-                .context("reading price history")?;
-            data_path.pop();
-
-            let now = time::OffsetDateTime::now_utc();
-            let current_price = history.price_at(now);
-            println!("Using price: {}", current_price);
-            println!("Using risk-free-rate: 4% (assumed)");
-
-            let years: f64 = (expiry - now) / time::Duration::days(365);
-            let price = black_scholes::call(
-                current_price.btc_price.to_f64().unwrap(),
-                strike.to_f64().unwrap(),
-                0.04f64, // risk free rate
-                volatility,
-                years,
-            );
-            println!("Price: {}", price);
-        }
-        Command::ImpliedVolatilityCall {
-            strike,
-            expiry,
-            price,
-            btc_price,
-        } => {
-            data_path.push("pricedata");
-            let history = Historic::read_json_from(&data_path, MIN_PRICE_DATE)
-                .context("reading price history")?;
-            data_path.pop();
-
-            println!("Call: strike {} exp {} price {}", strike, expiry, price);
-            println!("");
-
-            let now = time::OffsetDateTime::now_utc();
-            let current_price = btc_price
-                .map(|d| BitcoinPrice::from_current(d))
-                .unwrap_or(history.price_at(now));
-            println!("Using price: {:.2}", current_price);
-            println!("Using risk-free-rate: 4% (assumed)");
-
-            let years: f64 = (expiry - now) / time::Duration::days(365);
-            println!("Years to expiry: {:.8} (inv {:.6})", years, 1.0 / years);
-            let vol = black_scholes::call_iv(
-                price.to_f64().unwrap(),
-                current_price.btc_price.to_f64().unwrap(),
-                strike.to_f64().unwrap(),
-                0.04f64, // risk free rate
-                years,
-            )
-            .unwrap();
-            println!("Implied volatility: {:.6} %", 100.0 * vol);
-            let theta = black_scholes::call_theta(
-                current_price.btc_price.to_f64().unwrap(),
-                strike.to_f64().unwrap(),
-                0.04f64, // risk free rate
-                vol,
-                years,
-            ) / 365.0;
-            println!("Theta: {:.6}", theta);
-            println!(
-                "Annualized return if expires worthless: {:.6} %",
-                100.0
-                    * (1.0f64
-                        + price.to_f64().unwrap() / current_price.btc_price.to_f64().unwrap())
-                    .powf(1.0f64 / years)
-                    - 100.0
-            );
-        }
-        Command::PricePut {
-            strike,
-            expiry,
-            volatility,
-        } => {
-            data_path.push("pricedata");
-            let history = Historic::read_json_from(&data_path, MIN_PRICE_DATE)
-                .context("reading price history")?;
-            data_path.pop();
-
-            let now = time::OffsetDateTime::now_utc();
-            let current_price = history.price_at(now);
-            println!("Using price: {}", current_price);
-            println!("Using risk-free-rate: 4% (assumed)");
-
-            let years: f64 = (expiry - now) / time::Duration::days(365);
-            let price = black_scholes::put(
-                current_price.btc_price.to_f64().unwrap(),
-                strike.to_f64().unwrap(),
-                0.04f64, // risk free rate
-                volatility,
-                years,
-            );
-            println!("Price: {}", price);
-        }
-        Command::ImpliedVolatilityPut {
-            strike,
-            expiry,
-            price,
-            btc_price,
-        } => {
-            data_path.push("pricedata");
-            let history = Historic::read_json_from(&data_path, MIN_PRICE_DATE)
-                .context("reading price history")?;
-            data_path.pop();
-
-            println!("Put: strike {} exp {} price {}", strike, expiry, price);
-            println!("");
-
-            let now = time::OffsetDateTime::now_utc();
-            let current_price = btc_price
-                .map(|d| BitcoinPrice::from_current(d))
-                .unwrap_or(history.price_at(now));
-            println!("Using price: {}", current_price);
-            println!("Using risk-free-rate: 4% (assumed)");
-
-            let years: f64 = (expiry - now) / time::Duration::days(365);
-            println!("Years to expiry: {:.8} (inv {:.6})", years, 1.0 / years);
-            let vol = black_scholes::put_iv(
-                price.to_f64().unwrap(),
-                current_price.btc_price.to_f64().unwrap(),
-                strike.to_f64().unwrap(),
-                0.04f64, // risk free rate
-                years,
-            );
-            if let Ok(vol) = vol {
-                println!("Implied volatility: {:.6} %", 100.0 * vol);
-                let theta = black_scholes::put_theta(
-                    current_price.btc_price.to_f64().unwrap(),
-                    strike.to_f64().unwrap(),
-                    0.04f64, // risk free rate
-                    vol,
-                    years,
-                ) / 365.0;
-                println!("Theta: {:.6}", theta);
-            } else {
-                println!("Implied volatility: (cannot compute) %");
-                println!("Theta: (cannot compute) %");
-            }
-            println!(
-                "Annualized return if expires worthless: {:.6} %",
-                100.0
-                    * (1.0f64 + price.to_f64().unwrap() / strike.to_f64().unwrap())
-                        .powf(1.0f64 / years)
-                    - 100.0
-            );
         }
     }
 
